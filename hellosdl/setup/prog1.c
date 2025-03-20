c3acf491256803853abb718ca71b6b32f65e77c5
0x50-0x42 <arya.grafter@gmail.com>
2025-03-20 15:24:32 +0530

    setting up SDL2

---
+++ hellosdl/setup/prog1.c
@@ -0,0 +1,18 @@
+/******
+ * Program: prog1.c
+ * Description: setting up SDL2
+ *
+ * Note:
+ * to compile this source file, use:
+ *
+ * gcc prog1.c -w -lSDL2 -o prog1
+ ******/
+
+
+#include<SDL2/SDL.h>
+#include<stdio.h>
+
+int main(void) {
+	puts("hello world");
+	return 0;
+}
